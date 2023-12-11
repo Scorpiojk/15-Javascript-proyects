5a7333282320313cdc7b60a1d7dc3c92c5083cfe
ScorpioJK <josekolomenski@gmail.com>
2023-12-11 08:39:18 -0300

    Sixth project

--- Proyects/06-modal/app.js
+++ Proyects/06-modal/app.js
@@ -2,3 +2,13 @@
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
+
+const openModalBtn = document.querySelector(".modal-btn");
+const closeModalBtn = document.querySelector(".close-btn");
+const modal = document.querySelector(".modal-overlay");
+
+openModalBtn.addEventListener("click", () => modal.classList.add("open-modal"));
+
+closeModalBtn.addEventListener("click", () =>
+  modal.classList.remove("open-modal")
+);
