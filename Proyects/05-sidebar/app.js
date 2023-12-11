a99d0def41d5648eebe79efc5582aebab05ee4c3
ScorpioJK <josekolomenski@gmail.com>
2023-12-11 08:30:27 -0300

    Fifth project

---
+++ Proyects/05-sidebar/app.js
@@ -0,0 +1,12 @@
+// Select items
+const toggleBtn = document.querySelector(".sidebar-toggle");
+const closeBtn = document.querySelector(".close-btn");
+const sideBar = document.querySelector(".sidebar");
+
+toggleBtn.addEventListener("click", () =>
+  sideBar.classList.toggle("show-sidebar")
+);
+
+closeBtn.addEventListener("click", () =>
+  sideBar.classList.remove("show-sidebar")
+);
