b88bd12eec36e3191a7fac55a22d6287b1b73c79
ScorpioJK <josekolomenski@gmail.com>
2023-12-11 21:22:13 -0300

    Seventh project

--- Proyects/07-questions/app.js
+++ Proyects/07-questions/app.js
@@ -1,2 +1,29 @@
 //using selectors inside the element
+
+const questions = document.querySelectorAll(".question");
+
+questions.forEach((target) => {
+  const btn = target.querySelector(".question-btn");
+
+  btn.addEventListener("click", () => {
+    questions.forEach((item) => {
+      if (item !== target) {
+        item.classList.remove("show-text");
+      }
+    });
+
+    target.classList.toggle("show-text");
+  });
+});
+
 // traversing the dom
+/*
+const btns = document.querySelectorAll(".question-btn");
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    let article = e.currentTarget.parentElement.parentElement;
+    article.classList.toggle("show-text");
+  });
+});
+*/
