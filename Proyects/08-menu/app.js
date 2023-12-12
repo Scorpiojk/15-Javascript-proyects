2ebd2ae05a6f12ad06856767ad029d785a094b8d
ScorpioJK <josekolomenski@gmail.com>
2023-12-12 09:00:37 -0300

    Eight project

--- Proyects/08-menu/app.js
+++ Proyects/08-menu/app.js
@@ -1,3 +1,4 @@
+// List of items
 const menu = [
   {
     id: 1,
@@ -71,4 +72,83 @@ const menu = [
     img: "./images/item-9.jpeg",
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
+  {
+    id: 10,
+    title: "Steak",
+    category: "dinner",
+    price: 49.99,
+    img: "./images/item-10.jpeg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
 ];
+
+const section = document.querySelector(".section-center");
+const btnContainer = document.querySelector(".btn-container");
+
+// Display when loading
+window.addEventListener("DOMContentLoaded", () => {
+  displayMenuItems(menu);
+  displayMenuButtons();
+});
+
+displayMenuItems = (menuItems) => {
+  let display = menuItems.map((item) => {
+    return `
+    <article class="menu-item">
+    <img src=${item.img} alt=${item.title} class="photo" />
+    <div class="item-info">
+    <header>
+    <h4>${item.title}</h4>
+    <h4 class="price">$${item.price}</h4>
+    </header>
+    <p class="item-text">
+    ${item.desc}
+    </p>
+    </div>
+    </article>
+    `;
+  });
+  // Delete commas
+  display = display.join("");
+  section.innerHTML = display;
+};
+
+displayMenuButtons = () => {
+  const categories = menu.reduce(
+    (values, item) => {
+      if (!values.includes(item.category)) {
+        values.push(item.category);
+      }
+      return values;
+    },
+    ["all"]
+  );
+  const categoryBtns = categories
+    .map((category) => {
+      return `
+      <button class="filter-btn" type="button" data-id="${category}">
+      ${category}
+      </button>
+      `;
+    })
+    .join("");
+  btnContainer.innerHTML = categoryBtns;
+  const filterBtns = btnContainer.querySelectorAll(".filter-btn");
+
+  // filter items
+  filterBtns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      const category = e.currentTarget.dataset.id;
+      const menuCategory = menu.filter((menuItem) => {
+        if (menuItem.category === category) {
+          return menuItem;
+        }
+      });
+      if (category === "all") {
+        displayMenuItems(menu);
+      } else {
+        displayMenuItems(menuCategory);
+      }
+    });
+  });
+};
